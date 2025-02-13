08c2102fb741a6276f8506459969a3783cd1374f
anph <phamhain2001@gmail.com>
2025-02-13 17:25:15 +0700

    authen with jwt

---
+++ database/migrations/2025_02_12_154614_add_role_to_users_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Domain\Enums\UserRoles;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->enum('role', UserRoles::values())->default(UserRoles::Customer->value);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('role');
+        });
+    }
+};
